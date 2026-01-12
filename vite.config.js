b87dcd14887865acec5272e85750399152605379
Haru-Family <wwxk1593@gmail.com>
2026-01-12 19:44:01 +0900

    feat: 배포 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/Pocketmon/', // GitHub 저장소 이름과 동일하게 설정
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
                 hangman: resolve(__dirname, 'games/hangman/index.html'),
-                cry_quiz: resolve(__dirname, 'games/cry-quiz/index.html'),
-                card_flip: resolve(__dirname, 'games/card-flip/index.html'),
+                cryQuiz: resolve(__dirname, 'games/cry-quiz/index.html'),
+                cardFlip: resolve(__dirname, 'games/card-flip/index.html'),
             },
         },
     },
